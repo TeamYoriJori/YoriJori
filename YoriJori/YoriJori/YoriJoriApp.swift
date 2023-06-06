0fd769adf26496b393c5a7dd5f7a266017966cb2
forest.jay <forest.jay@kakaocorp.com>
2023-06-06 21:32:28 +0900

    Initial Commit

---
+++ YoriJori/YoriJori/YoriJoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoriJoriApp.swift
+//  YoriJori
+//
+//  Created by forest on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct YoriJoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
