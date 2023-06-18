65227fcbb3347da2379618d0b5252ef7e6699e5d
yeahg-dev <yeahg.dev@gmail.com>
2023-06-18 12:51:50 +0900

    feat: PermanentStorageManager 인터페이스 정의

--- YoriJori/YoriJori/PermanentStorage/PermanentStorageManager.swift
+++ YoriJori/YoriJori/PermanentStorage/PermanentStorageManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol PermanentStorageManager {
+    
+    func create<T>(entity: T) throws -> Void
+    func fetch<T>(identifier: UUID) throws -> T?
+    func fetchAll<T>() throws -> [T]
+    func fetchAll<T>(by:[Condition]) throws -> [T]
+    func fetchAll<T>(sortedBy: [Criteria]) throws -> [T]
+    func update<T>(entity: T) throws -> Void
+    func delete<T>(entity: T) throws -> Void
+    func deleteAll<T>(entityType: T) throws -> Void
+    
+}
+
+protocol Condition {}
+
+protocol Criteria {}
