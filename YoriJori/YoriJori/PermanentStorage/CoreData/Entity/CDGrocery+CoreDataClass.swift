db5fdab5aec4f8b545d00d3f2577f962e9a333e8
yeahg-dev <yeahg.dev@gmail.com>
2023-06-25 16:44:00 +0900

    feat: Grocery 타입 정의

---
+++ YoriJori/YoriJori/PermanentStorage/CoreData/Entity/CDGrocery+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDGrocery+CoreDataClass.swift
+//  YoriJori
+//
+//  Created by Moon Yeji on 2023/06/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDGrocery)
+public class CDGrocery: NSManagedObject {
+
+}
