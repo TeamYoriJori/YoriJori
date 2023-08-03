bb014fab02f2af86735009a4615c9832595a8b7b
forest.jay <forestsj30@gmail.com>
2023-08-03 19:53:36 +0900

    feat: RecipeBookRepository 프로토콜 작성

---
+++ YoriJori/YoriJori/Repository/Recipe/RecipeBookRepositoryProtocol.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeBookRepository.swift
+//  YoriJori
+//
+//  Created by forest on 2023/08/03.
+//
+
+import Foundation
+
+protocol RecipeBookRepositoryProtocol {
+    func fetchAllRecipeBooks() throws -> [RecipeBook]
+    func fetchAllRecipeBooks(by sorts: [RecipeBookSortDescriptor: Bool]) throws -> [RecipeBook]
+    func fetchRecipeBookByID(_ id: UUID) throws -> RecipeBook?
+    func fetchRecipeBooksByTitle(_ title: String, sorts: [RecipeBookSortDescriptor: Bool]) throws -> [RecipeBook]
+    func createRecipeBook(_ model: RecipeBook) throws
+    func updateRecipeBook(_ recipeBook: RecipeBook) throws
+    func deleteRecipeBook(_ recipeBook: RecipeBook) throws
+}
+
+enum RecipeBookSortDescriptor: String {
+    case titleAscending = "title"
+    case updatedAtAscending = "updatedAt"
+}
