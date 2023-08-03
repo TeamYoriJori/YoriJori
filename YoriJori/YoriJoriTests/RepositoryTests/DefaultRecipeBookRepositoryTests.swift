87d2410a6fa4040f1cd447d5d5aab048a6309db4
forest.jay <forestsj30@gmail.com>
2023-08-03 20:18:53 +0900

    test: RecipeBookRepository 테스트케이스 추가

--- YoriJori/YoriJoriTests/RepositoryTests/DefaultRecipeBookRepositoryTests.swift
+++ YoriJori/YoriJoriTests/RepositoryTests/DefaultRecipeBookRepositoryTests.swift
@@ -34,4 +34,81 @@ final class DefaultRecipeBookRepositoryTests: XCTestCase {
         let expectedRecipe = try? sut.fetchRecipeBooksByTitle("한식", sorts: [:])
         XCTAssertEqual(recipeBook, expectedRecipe?.first)
     }
+    
+    func test_deleteRecipeBook()  throws {
+        // Arrange
+        let recipeBook = DummyRecipeBook.korean
+        
+        // Act
+        try sut.createRecipeBook(recipeBook)
+        try sut.deleteRecipeBook(recipeBook)
+        
+        // Assert
+        let expectedRecipe = try? sut.fetchRecipeBooksByTitle("한식", sorts: [:])
+        XCTAssertNil(expectedRecipe?.first)
+    }
+    
+    func test_updateRecipeBook() throws {
+        // Arrange
+        let recipeBook = DummyRecipeBook.korean
+        let recipeBookUpdatedName = RecipeBook(
+            id: recipeBook.id,
+            title: "한국음식",
+            image: nil,
+            updatedAt: Date(),
+            recipes: []
+        )
+        // Act
+        try sut.createRecipeBook(recipeBookUpdatedName)
+        try sut.updateRecipeBook(recipeBookUpdatedName)
+        
+        // Assert
+        let expectedRecipe = try? sut.fetchRecipeBookByID(recipeBookUpdatedName.id)
+        XCTAssertEqual(recipeBookUpdatedName.title, expectedRecipe?.title)
+    }
+    
+    func test_fetchAllRecipeBook() throws {
+        // Arrange
+        let firstRecipeBook = DummyRecipeBook.korean
+        let secondRecipeBook = DummyRecipeBook.japanese
+        
+        try sut.createRecipeBook(firstRecipeBook)
+        try sut.createRecipeBook(secondRecipeBook)
+        
+        // Act
+        let result = try sut.fetchAllRecipeBooks()
+        
+        // Assert
+        XCTAssertEqual(result.count, 2)
+    }
+    
+    func test_fetchAllRecipeBooksWithUpdatedAtSorting() throws {
+        // Arrange
+        let firstRecipeBook = DummyRecipeBook.korean
+        let secondRecipeBook = DummyRecipeBook.japanese
+        
+        try sut.createRecipeBook(firstRecipeBook)
+        try sut.createRecipeBook(secondRecipeBook)
+        
+        // Act
+        let result = try sut.fetchAllRecipeBooks(by: [.updatedAtAscending: true])
+        
+        // Assert
+        XCTAssertEqual(result, [firstRecipeBook, secondRecipeBook])
+    }
+    
+    func test_fetchAllRecipeBooksWithTitleSorting() throws {
+        // Arrange
+        let koreanRecipeBook = DummyRecipeBook.korean
+        let japaneseRecipeBook = DummyRecipeBook.japanese
+        
+        try sut.createRecipeBook(koreanRecipeBook)
+        try sut.createRecipeBook(japaneseRecipeBook)
+        
+        // Act
+        let result = try sut.fetchAllRecipeBooks(by: [.titleAscending: true])
+        
+        // Assert
+        XCTAssertEqual(result, [japaneseRecipeBook, koreanRecipeBook])
+    }
 }
