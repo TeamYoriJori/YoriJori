7dfa4d959210b01bff41bac48053afd3b436c8c5
Lily <yeahg.dev@gmail.com>
2024-09-08 23:51:39 +0900

    RecipeInfo뷰에 StepProgressView 추가

--- YoriJori/YoriJori/View/WriteRecipe/RecipeInformationView.swift
+++ YoriJori/YoriJori/View/WriteRecipe/RecipeInformationView.swift
@@ -25,71 +25,74 @@ struct RecipeInformationView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 28, content: {
-                VStack(alignment: .leading, content: {
-                    Text("레시피 이름")
-                    VStack(content: {
-                        TextField("", text: $name, prompt: Text("요리 이름을 입력해주세요"))
-                            .focused($focusedField, equals: .name)
-                            .onSubmit { focusedField = .nickname }
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
+        VStack(spacing: 20, content: {
+            StepProgressView(totalStepCount: 4, currnetStep: 1)
+            ScrollView {
+                VStack(spacing: 28, content: {
+                    VStack(alignment: .leading, content: {
+                        Text("레시피 이름")
+                        VStack(content: {
+                            TextField("", text: $name, prompt: Text("요리 이름을 입력해주세요"))
+                                .focused($focusedField, equals: .name)
+                                .onSubmit { focusedField = .nickname }
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        })
                     })
-                })
-                VStack(alignment: .leading, content: {
-                    Text("레시피 별명")
-                    VStack(content: {
-                        TextField("", text: $nickname, prompt: Text("#5분요리"))
-                            .focused($focusedField, equals: .nickname)
-                            .onSubmit { focusedField = .tag }
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
+                    VStack(alignment: .leading, content: {
+                        Text("레시피 별명")
+                        VStack(content: {
+                            TextField("", text: $nickname, prompt: Text("#5분요리"))
+                                .focused($focusedField, equals: .nickname)
+                                .onSubmit { focusedField = .tag }
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        })
                     })
-                })
-                VStack(alignment: .leading, content: {
-                    Text("태그")
-                    VStack(content: {
-                        TextField("", text: $tag, prompt: Text("요리 별명을 만들어주세요"))
-                            .focused($focusedField, equals: .tag)
-                            .onSubmit { focusedField = .recipeBook }
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
+                    VStack(alignment: .leading, content: {
+                        Text("태그")
+                        VStack(content: {
+                            TextField("", text: $tag, prompt: Text("요리 별명을 만들어주세요"))
+                                .focused($focusedField, equals: .tag)
+                                .onSubmit { focusedField = .recipeBook }
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        })
                     })
-                })
-                VStack(alignment: .leading, content: {
-                    Text("레시피 북")
-                    VStack(content: {
-                        TextField("", text: $recipeBook, prompt: Text("아침"))
-                            .focused($focusedField, equals: .recipeBook)
-                        Rectangle()
-                            .frame(height: 1)
-                            .foregroundColor(.gray)
+                    VStack(alignment: .leading, content: {
+                        Text("레시피 북")
+                        VStack(content: {
+                            TextField("", text: $recipeBook, prompt: Text("아침"))
+                                .focused($focusedField, equals: .recipeBook)
+                            Rectangle()
+                                .frame(height: 1)
+                                .foregroundColor(.gray)
+                        })
                     })
-                })
-                VStack(alignment: .leading, spacing: 8, content: {
-                    HStack {
-                        Text("대표 이미지")
-                        Spacer()
-                        PhotosPicker(selection: $image.imageSelection,
-                                     matching: .images,
-                                     photoLibrary: .shared()) {
-                            Text( image.imageSelection == nil ? "선택하기": "다시 선택하기")
-                                .foregroundColor(.orange)
+                    VStack(alignment: .leading, spacing: 8, content: {
+                        HStack {
+                            Text("대표 이미지")
+                            Spacer()
+                            PhotosPicker(selection: $image.imageSelection,
+                                         matching: .images,
+                                         photoLibrary: .shared()) {
+                                Text( image.imageSelection == nil ? "선택하기": "다시 선택하기")
+                                    .foregroundColor(.orange)
+                            }
                         }
-                    }
-                    GeometryReader(content: { geometry in
-                        PhotoPickerImagePresenterView(imageState: image.imageState)
-                            .frame(width: geometry.size.width, height: geometry.size.width * 3/4)
-                            .background(.gray)
+                        GeometryReader(content: { geometry in
+                            PhotoPickerImagePresenterView(imageState: image.imageState)
+                                .frame(width: geometry.size.width, height: geometry.size.width * 3/4)
+                                .background(.gray)
+                        })
                     })
                 })
-            })
-            .padding(.horizontal, 20)
-        }
+                .padding(.horizontal, 20)
+            }
+        })
     }
 }
 
