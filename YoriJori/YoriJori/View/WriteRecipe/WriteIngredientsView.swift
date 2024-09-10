a6e5bbbc94c358dc75cb111209afe3f7750dafe0
Lily <yeahg.dev@gmail.com>
2024-09-10 23:32:35 +0900

    WriteIngredientsView로 네비게이션 연결

---
+++ YoriJori/YoriJori/View/WriteRecipe/WriteIngredientsView.swift
@@ -0,0 +1,22 @@
+//
+//  WriteIngredientsView.swift
+//  YoriJori
+//
+//  Created by 예지 on 9/10/24.
+//
+
+import SwiftUI
+
+struct WriteIngredientsView: View {
+    
+    var body: some View {
+        VStack {
+            StepProgressView(totalStepCount: 4, currnetStep: 2)
+        }
+    }
+    
+}
+
+#Preview {
+    WriteIngredientsView()
+}
