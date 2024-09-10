e75e26c93dae2a2d8ec26bf1c6272c1c7a61b96e
Lily <yeahg.dev@gmail.com>
2024-09-10 23:11:04 +0900

    RecipeStorage todo 작성

---
+++ YoriJori/YoriJori/Domain/RecipeStorage.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeStorage.swift
+//  YoriJori
+//
+//  Created by 예지 on 9/10/24.
+//
+
+import Foundation
+
+final class RecipeStorage {
+    
+    // getEmptyRecipe
+    
+    // getDraftRecipe
+    
+}
