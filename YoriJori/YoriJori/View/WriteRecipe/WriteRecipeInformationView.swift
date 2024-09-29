76491e20d59cb3848ad62951eca1c45258a0d7dd
Lily <yeahg.dev@gmail.com>
2024-09-29 12:53:22 +0900

    요리 재료 작성 버튼 레이아웃 조정

--- YoriJori/YoriJori/View/WriteRecipe/WriteRecipeInformationView.swift
+++ YoriJori/YoriJori/View/WriteRecipe/WriteRecipeInformationView.swift
@@ -104,8 +104,8 @@ struct WriteRecipeInformationView: View {
                     ZStack {
                         Rectangle()
                             .fill(.orange)
-                        Text("요리 재료 작성")
                             .frame(height: 64)
+                        Text("요리 재료 작성")
                             .foregroundStyle(.white)
                     }
                 }
